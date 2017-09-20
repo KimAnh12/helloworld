7dba8a9b4352d5b160d7a7d9871a270663c33820
KimAnh12 <31847974+KimAnh12@users.noreply.github.com>
2017-09-20 21:09:11 +0700

    Source code

---
+++ Hello World/Hello World/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    public partial class frmHelloWorld : Form
+    {
+        public frmHelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+            lblHelloWorld.Left = (ClientSize.Width - lblHelloWorld.Width) / 2;
+            lblHelloWorld.Top = (ClientSize.Height - lblHelloWorld.Height) / 2;
+
+
+        }
+    }
+}
